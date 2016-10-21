f07ff2c676e4cb7346b7efdb138aaf608e24f5eb
Deo Bissessar <Deo Bissessar>
2016-10-20 21:00:31 -0400

    edited index.js for heroku upload

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path      = require("path");
 var Sequelize = require("sequelize");
 var env       = process.env.NODE_ENV || "development";
 var config    = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+//var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db        = {};
 
 if(config.use_env_variable){
