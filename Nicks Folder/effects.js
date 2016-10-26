ed3728a40097b9e6021f14d6408e57823dc070ff
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-10-25 21:35:48 -0400

    nicks folder

---
+++ Nicks Folder/effects.js
@@ -0,0 +1,23 @@
+
+$( document ).ready(function() {
+	$(".button-collapse").sideNav();
+    $('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 240
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    }
+  );
+    $(".dropdown-button").dropdown();
+    
+});
+  // Show sideNav
+  $('.button-collapse').sideNav('show');
+  // Hide sideNav
+  $('.button-collapse').sideNav('hide');
+
+
+
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
